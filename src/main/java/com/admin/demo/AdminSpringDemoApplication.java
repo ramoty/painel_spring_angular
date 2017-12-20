3b32ec7c575554c51177f08f984b796ffd36cfff
Alison Moura <alison.oghino@gmail.com>
2017-12-20 00:05:15 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/admin/demo/AdminSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.admin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminSpringDemoApplication.class, args);
+	}
+}
